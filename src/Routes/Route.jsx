5573eb267349a5e4dde3a3dc8997e0b819318876
Habibur Rahman <habib.swi16@gmail.com>
2023-09-20 17:06:08 +0600

    home page

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../pages/Home/Home";
+import Products from "../pages/Products/Products";
+import Dashboard from "../pages/Dashboard/Dashboard";
+
+const Route = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+        
+      },
+    ],
+  },
+]);
+
+export default Route;
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import MainLayout from "./Layouts/MainLayout";
-import Home from "./pages/Home/Home";
-import Products from "./pages/Products/Products";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/products",
-        element: <Products />,
-      },
-    ],
-  },
-]);
+import Route from "./routes/Route";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={Route}></RouterProvider>
   </React.StrictMode>
 );
